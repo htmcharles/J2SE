d9cc1309dbe46b02a52b0e1930741bdfec673284
htmcharles <hatumacharles1@gmail.com>
2024-10-31 15:16:36 +0200

    readfile

--- intro/Program.java
+++ intro/Program.java
@@ -1,7 +1,5 @@
-public Class Program{
-  public static void main (String[] args){
-  System.out.println("Good afternoon" + " " +args[0] + " "+ args[1]);
-
-}
-
+public class Program {
+  public static void main(String[] args) {
+    System.out.println("Good afternoon" + " " + args[0] + " " + args[1]);
+  }
 }
